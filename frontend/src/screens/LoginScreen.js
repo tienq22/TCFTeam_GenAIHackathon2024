db9821efaf6e6a76c1fe2b82dd838a0c7c92601e
Tiến_Nq <tiennq310@gmail.com>
2024-06-30 11:47:31 +0700

    [FE] js, css of Update LoginScreen & HomeScreen

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,14 +1,13 @@
-
 import { useState, useEffect } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
+import FormContainer from "../components/FormContainer.js";
 import Loader from "../components/Loader";
 import { useLoginMutation } from "../slices/usersApiSlice";
 import { setCredentials } from "../slices/authSlice";
 import { toast } from "react-toastify";
 import "./styles/logintrue.css";
-
 
 const LoginScreen = () => {
   const [email, setEmail] = useState("");
@@ -43,58 +42,59 @@ const LoginScreen = () => {
   };
 
   return (
-    <div className="login-wrapper">
-      <div className="login-container">
-      <div className="login-header">
-        <h1>Đăng nhập ngay!!</h1>
-        <p>Chào mừng bạn quay trở lại 👋</p>
-        </div>
-        <Form onSubmit={submitHandler}>
-          <Form.Group controlId="formEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Nhập địa chỉ Email của bạn"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="custom-placeholder"
-            />
-          </Form.Group>
-
-          <Form.Group controlId="formPassword">
-            <Form.Label>Mật khẩu</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Nhập mật khẩu của bạn"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="custom-placeholder"
-            />
-          </Form.Group>
-
-          <div className="d-flex justify-content-between align-items-center">
-            <Form.Check type="checkbox" label="Ghi nhớ mật khẩu" />
-            <a href="#forgot-password" className="forgot-password">Quên mật khẩu?</a>
+    <FormContainer>
+      <div className="login-wrapper">
+        <div className="login-container">
+          <div className="login-header">
+            <h1>Đăng nhập ngay!!</h1>
+            <p>Chào mừng bạn quay trở lại 👋</p>
           </div>
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId="formEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Nhập địa chỉ Email của bạn"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="custom-placeholder"
+              />
+            </Form.Group>
 
+            <Form.Group controlId="formPassword">
+              <Form.Label>Mật khẩu</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Nhập mật khẩu của bạn"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="custom-placeholder"
+              />
+            </Form.Group>
 
-          <Button variant="primary" type="submit" className="btn-login">
-            Đăng Nhập
-          </Button>
+            <div className="d-flex justify-content-between align-items-center">
+              <Form.Check type="checkbox" label="Ghi nhớ mật khẩu" />
+              <a href="#forgot-password" className="forgot-password">Quên mật khẩu?</a>
+            </div>
 
-          {isLoading && <Loader />}
+            <Button variant="primary" type="submit" className="btn-login">
+              Đăng Nhập
+            </Button>
+
+            {isLoading && <Loader />}
+          </Form>
 
           <Row className="py-3">
             <Col className="register-link">
               Nếu bạn chưa có tài khoản? <Link to="/register">Đăng ký</Link>
             </Col>
           </Row>
-        </Form>
+        </div>
+        <div className="image-container">
+          <img src="/login-image.png" alt="Login Illustration" />
+        </div>
       </div>
-      <div className="image-container">
-        <img src="/login-image.png" alt="Login Illustration" />
-      </div>
-    </div>
+    </FormContainer>
   );
 };
 
