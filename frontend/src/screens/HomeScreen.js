6ceba03321e9b4d1660e6879ab7df7867a8b3d97
Tiến_Nq <tiennq310@gmail.com>
2024-07-01 11:30:32 +0700

    Updae css

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,29 +1,26 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from "react"; // Thêm import useState từ React
 import { Button, Container, Row, Col } from 'react-bootstrap';
 import { toast } from "react-toastify";
 import { useCreateExamMutation } from "../slices/examsApiSlice";
-import Loader from '../components/Loader'; // Đảm bảo bạn đã nhập đúng đường dẫn của Loader
 import "./styles/homeScreen.css";
 
 
 const HomeScreen = () => {
   const navigate = useNavigate();
   const [createExam] = useCreateExamMutation();
-  const [loading, setLoading] = useState(false); // Thêm state để quản lý trạng thái loading
 
   const submitHandler = async (e) => {
     e.preventDefault();
-    setLoading(true); // Bắt đầu loading khi submit
     try {
       //const { examId } = await createExam().unwrap();
+
       const examId = "668187dcc2b219223c851f64"
+      
       if (examId) {
         navigate(`/exams/${examId}`);
         toast.success("Exam created successfully");
       }
     } catch (error) {
-      setLoading(false); // Dừng loading nếu có lỗi
       toast.error(error?.data?.message);
     }
   };
@@ -43,6 +40,7 @@ const HomeScreen = () => {
       <Row className="justify-content-center text-center">
         <Col>
           <Button className="btn-start" onClick={submitHandler}>Làm bài ngay</Button>
+          { {examId && <Loader />} }
         </Col>
       </Row>
       <Row className="justify-content-center">
--- frontend/src/screens/TakeExamScreen.js
+++ frontend/src/screens/TakeExamScreen.js
@@ -1,121 +1,40 @@
-<<<<<<< HEAD
-// import React from "react";
-// import { useNavigate, useParams } from "react-router-dom";
-// import Loader from "../components/Loader";
-// import Message from "../components/Message";
-// import { useGetExamDetailsQuery } from "../slices/examsApiSlice";
-
-// const TakeExamScreen = () => {
-//   const navigate = useNavigate();
-//   const { id: examId } = useParams();
-//   const { data: examData, isLoading, error } = useGetExamDetailsQuery(examId);
-
-//   // const Question = () => {
-//   //   const questionElements = [];
-//   // for (let i = 0; i < questions.length; i++) {
-//   //   questionElements.push(
-//   //     <div key={i}>
-//   //       <h3>Question {i + 1}</h3>
-//   //       <p>{questions[i].question}</p>
-//   //       <p>Options: {questions[i].options.join(', ')}</p>
-//   //       <p>Correct Option: {questions[i].correctOption}</p>
-//   //       <p>Answer Detail: {questions[i].answerDetail}</p>
-//   //     </div>
-//   //   );
-//   // }
-  
-
-
-//   return isLoading ? (
-//     <Loader />
-//   ) : error ? (
-//     <Message variant='danger'>
-//       {error?.status} {JSON.stringify(error?.data)}
-//     </Message>
-//   ) : (
-//     <div>
-//       <div>
-//         <h1>Đề bài</h1>
-//       </div>
-//       <div>
-//         <text>{examData.parts.part1.questionType}</text> <br></br>
-//         <text>{examData.parts.part1.questions[0].question}</text>
-//         <ul class="">
-//           <li>{examData.parts.part1.questions[0].options[0]}</li>
-//           <li>{examData.parts.part1.questions[0].options[1]}</li>
-//           <li>{examData.parts.part1.questions[0].options[2]}</li>
-//           <li>{examData.parts.part1.questions[0].options[3]}</li>
-//         </ul>
-//         <br></br>
-//         <text>{examData.parts.part1.questions[1].question}</text>
-//         <ul class="">
-//           <li>{examData.parts.part1.questions[1].options[0]}</li>
-//           <li>{examData.parts.part1.questions[1].options[1]}</li>
-//           <li>{examData.parts.part1.questions[1].options[2]}</li>
-//           <li>{examData.parts.part1.questions[1].options[3]}</li>
-//         </ul>
-//         {/* <text>{questionElements}</text> */}
-
-        
-//       </div>
-//     </div>
-
-//   );
-
-// };
-
-// export default TakeExamScreen;
-
-import React from "react";
-=======
-import React, { useState, useEffect } from "react";
->>>>>>> b5526bc ([FE] Update js & css fol HomeScreen, LoginScreen, TakeExamScreen])
+//import React from "react";
+import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from "react-router-dom";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import { useGetExamDetailsQuery } from "../slices/examsApiSlice";
-<<<<<<< HEAD
-=======
 import './styles/takeExamScreen.css';
->>>>>>> b5526bc ([FE] Update js & css fol HomeScreen, LoginScreen, TakeExamScreen])
+import CountdownTimer from '../slices/CounterDownTimer';
+
+
 
 const TakeExamScreen = () => {
   const navigate = useNavigate();
   const { id: examId } = useParams();
   const { data: examData, isLoading, error } = useGetExamDetailsQuery(examId);
+  const [answers, setAnswers] = useState(Array(50).fill(null));
 
-<<<<<<< HEAD
-=======
-  const [timeLeft, setTimeLeft] = useState(3600); // Set thời gian làm bài là 1 giờ (3600 giây)
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTimeLeft((prevTime) => prevTime > 0 ? prevTime - 1 : 0);
-    }, 1000);
-
-    return () => clearInterval(timer); // Cleanup interval on component unmount
-  }, []);
-
-  const formatTime = (seconds) => {
-    const minutes = Math.floor(seconds / 60);
-    const secs = seconds % 60;
-    return `${minutes}:${secs < 10 ? '0' : ''}${secs}`;
-  };
 
   function renderQuestions(questions) {
     return questions.map((question, index) => (
-      <div key={index} className="question-item">
+      <div key={index}>
         <p>{question.question}</p>
         <ul className="options">
           {question.options.map((option, i) => (
-            <li key={i} className="option-item">{option}</li>
+            <li key={i}>{option}</li>
           ))}
         </ul>
       </div>
     ));
   }
 
->>>>>>> b5526bc ([FE] Update js & css fol HomeScreen, LoginScreen, TakeExamScreen])
+  const handleAnswerChange = (questionNum, option) => {
+    const updatedAnswers = [...answers];
+    updatedAnswers[questionNum] = option; // Assuming questionId starts from 1
+    setAnswers(updatedAnswers);
+  };
+ 
   return isLoading ? (
     <Loader />
   ) : error ? (
@@ -123,50 +42,83 @@ const TakeExamScreen = () => {
       {error?.status} {JSON.stringify(error?.data)}
     </Message>
   ) : (
-<<<<<<< HEAD
-    <>Exam data load successfully</>
-=======
     <div className="exam-container">
       <div className="header">
-        <h1 className="title">MÃ ĐỀ THI: E001-2024</h1>
+        <h1 className="title">Đề bài</h1>
         <div className="user-info">
-          <span className="user-icon">User</span>
-          <button className="logout-btn">KẾT THÚC</button>
+          <button className="logout-btn">THOÁT</button>
         </div>
       </div>
       <div className="exam-content">
-        <div className="question-section">
-          <div className="question-scroll">
-            {Object.keys(examData.parts).map((partKey) => (
-              <div key={partKey}>
-                <text className="questionType">{examData.parts[partKey].questionType}</text>
-                {renderQuestions(examData.parts[partKey].questions)}
-              </div>
-            ))}
-          </div>
+        <div className="question-section sidebar">
+          <text className="questionType">{examData.parts.part1.questionType}</text>
+          {renderQuestions(examData.parts.part1.questions)}
+
+          <text className="questionType">{examData.parts.part2.questionType}</text>
+          {renderQuestions(examData.parts.part2.questions)}
+
+          <text className="questionType">{examData.parts.part3.questionType}</text>
+          {renderQuestions(examData.parts.part3.questions)}
+
+          <text className="questionType">{examData.parts.part4.questionType}</text>
+          {renderQuestions(examData.parts.part4.questions)}
+
+          <text className="questionType">{examData.parts.part5.questionType}</text>
+          {renderQuestions(examData.parts.part5.questions)}
+
+          <text className="questionType">{examData.parts.part6.questionType}</text>
+          {renderQuestions(examData.parts.part6.questions)}
+
+          <text className="questionType">{examData.parts.part7.questionType}</text>
+          <text>{examData.parts.part7.passage}</text>
+          {renderQuestions(examData.parts.part7.questions)}
+
+          <text className="questionType">{examData.parts.part8.questionType}</text>
+          <text>{examData.parts.part7.passage}</text>
+          {renderQuestions(examData.parts.part8.questions)}
+          <text className="questionType">{examData.parts.part9.questionType}</text>
+          <text>{examData.parts.part9.passages[0]}</text>
+          <text>{examData.parts.part9.passages[1]}</text>
+          {renderQuestions(examData.parts.part9.questions)}
+
+          <text className="questionType">{examData.parts.part10.questionType}</text>
+          {renderQuestions(examData.parts.part10.questions)}
+
+          <text className="questionType">{examData.parts.part11.questionType}</text>
+          {renderQuestions(examData.parts.part11.questions)}
+
+          <text className="questionType">{examData.parts.part12.questionType}</text>
+          {renderQuestions(examData.parts.part12.questions)}
+
         </div>
         <div className="sidebar">
           <div className="timer">
             <span>Thời gian còn lại:</span>
-            <div className="time">{formatTime(timeLeft)}</div>
+            <div className="time:">{<CountdownTimer/>}</div>
           </div>
           <button className="submit-btn">NỘP BÀI</button>
           <div className="question-list question-list-scroll">
-            {[...Array(10).keys()].map((num) => (
-              <div key={num} className="question-item-right">
-                <span className="question-number">{num + 1}</span>
-                <div className="options-right">
-                  {['A', 'B', 'C', 'D'].map((option) => (
-                    <button key={option} className="option-button-right">{option}</button>
+            {
+            [...Array(50).keys()].map((num) => (
+              <div key={num} className="question-item">
+                <span>{num + 1}</span>
+                <div className="options">
+                  {['A', 'B', 'C', 'D'].map((option,i) => (
+                    <button  
+                    key={option}                   
+                    OnClick={() => handleAnswerChange(num+1, i) }
+                  >
+                    {option}
+                    </button>
                   ))}
                 </div>
               </div>
             ))}
+             
           </div>
         </div>
       </div>
     </div>
->>>>>>> b5526bc ([FE] Update js & css fol HomeScreen, LoginScreen, TakeExamScreen])
   );
 };
 
